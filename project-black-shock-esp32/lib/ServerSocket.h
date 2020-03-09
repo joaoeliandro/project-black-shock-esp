e14f3dc3ff87e86f86e9c10376005db01bb5c6b9
João Eliandro Germano <52173525+whistlerip@users.noreply.github.com>
2020-03-09 20:39:38 -0300

    Identação

--- project-black-shock-esp32/lib/ServerSocket.h
+++ project-black-shock-esp32/lib/ServerSocket.h
@@ -1,8 +1,8 @@
-#include<ArduinoWebsockets.h>
+#include <ArduinoWebsockets.h>
 
-const char* ssid = "Black Shock";
-const char* password = "blackshock";
-const char* websockets_server_host = "192.168.0.100";
+const char *ssid = "Black Shock";
+const char *password = "blackshock";
+const char *websockets_server_host = "192.168.0.100";
 const uint16_t websockets_server_port = 8080;
 
 using namespace websockets;
